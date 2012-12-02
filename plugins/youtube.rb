ddb7dbf2e8af9441f49e3cb2cc145abe4901d93a
Audio <horkej@hotmail.com>
2012-12-02 17:43:58 +0100

    YouTube addon: implement result caching

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -11,12 +11,21 @@ class Youtube
   match LONG_URL, use_prefix: false
   match SHORT_URL, use_prefix: false
 
+  def initialize(*args)
+    super
+    @cache = {}
+  end
+
   def execute(m, video_id)
     m.reply('YouTube: ' + title(video_id) )
   end
 
   def title(video_id)
-    doc = Nokogiri::XML( open('http://gdata.youtube.com/feeds/api/videos/' + video_id) )
-    doc.xpath('//media:title').first.content
+    unless @cache.has_key?(video_id)
+      doc = Nokogiri::XML( open('http://gdata.youtube.com/feeds/api/videos/' + video_id) )
+      @cache[video_id] = doc.xpath('//media:title').first.content
+    end
+
+    @cache[video_id]
   end
 end
