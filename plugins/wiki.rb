aad16429ccac7e331a862b9478fd31e71700e3e8
Audio <horkej@hotmail.com>
2013-02-02 17:16:58 +0100

    Wiki plugin: parse classic HTML page

--- plugins/wiki.rb
+++ plugins/wiki.rb
@@ -24,29 +24,38 @@ class Wiki
     result = result(lang, query)
     if result.nil?
       search_url = "http://#{lang}.wikipedia.org/w/api.php?action=opensearch&search=#{CGI.escape(query)}&limit=1&namespace=0&format=json"
-      json_search = WebPage.load_and_read_json(search_url)
+      json_search = WebPage.load_json(search_url)
       search_result = json_search[1][0]
       result = result(lang, search_result)
     end
 
     raise Exception if result.nil?
 
-    result_content = result[:revisions][0][:*]
-    result_line = result_content[/\n('{3}(\[{2})?)?[A-Z].[^\.:]*:?/]
-    last_char = result_line[-1,1]
-    result_line = result_content[/\* .+/] if last_char == ":"
-
-    result_line.gsub!(/(\[.*\|)|[\[\]']|(\* )/, "")
-    "#{result_line.strip} - http://#{lang}.wikipedia.org/wiki/#{result[:title]}"
-  rescue Exception => e
+    "#{first_sentense_of result} - http://#{lang}.wikipedia.org/wiki/#{query}"
+  rescue Exception
     "No result"
   end
 
   def result(lang, query)
-    url = "http://#{lang}.wikipedia.org/w/api.php?action=query&titles=#{CGI.escape(query)}&prop=revisions&rvprop=content&format=json"
-    json_exact = WebPage.load_and_read_json(url)
-    results = json_exact[:query][:pages]
-    pageid = results.keys[0]
-    pageid == :"-1" ? nil : results[pageid]
+    url = "http://#{lang}.wikipedia.org/wiki/#{CGI.escape(query)}"
+    doc = WebPage.load_html(url)
+    doc.xpath("//div[@id='bodyContent']/div/p[1]").text
+  end
+
+  def first_sentense_of(string)
+    string = remove_references string
+    sentenses = string.split '. '
+    result = "#{sentenses.shift}. "
+    while sentenses.size > 0
+      next_sentense = sentenses.shift
+      break if next_sentense.strip.match /^[A-Z]/
+      result << next_sentense << '. '
+    end
+
+    result
+  end
+
+  def remove_references(string)
+    string.gsub(/\[[\d]{1,2}\]/, '')
   end
 end
