bcb4c1ca02cd3b5f3bf98d1c83c5a6d211701e74
Audio <horkej@hotmail.com>
2013-02-09 21:52:50 +0100

    Title plugin: accept an additional text after the link itself

--- plugins/title.rb
+++ plugins/title.rb
@@ -6,7 +6,7 @@ class Title
 
   set :help, 't [url] - prints the title of a page specified in parameter. Example: t http://www.seznam.cz/'
 
-  match /t +(http.+)/
+  match /t +(http\S+)/
 
   def execute(m, url)
     m.reply( WebPage.load_html(url).title )
