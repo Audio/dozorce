94031ce7f0c5d7d49d4806657f55e697c9d077f4
Audio <horkej@hotmail.com>
2013-02-15 18:41:43 +0100

    Csfd plugin: robust parsing of not well-formed documents

--- plugins/csfd.rb
+++ plugins/csfd.rb
@@ -16,23 +16,24 @@ class Csfd
   def search(query)
     search_url = "http://csfd.matousskala.cz/api/hledat.php?q=#{CGI.escape(query)}"
     search_result = WebPage.load_xml(search_url)
-    search_ids = search_result.xpath("//film//id").first
-    raise "No result" if search_ids.nil?
+    movie_id = search_result.xpath("//film[1]/id").text
+    raise NoMovieFoundError if movie_id.empty?
 
-    movie_id = search_ids.content
     movie_url = "http://csfd.matousskala.cz/api/film.php?id=#{movie_id}"
     movie_result = WebPage.load_xml(movie_url)
 
-    movie_title = movie_result.xpath("//nazev").first.content
-    made_data = movie_result.xpath("//zeme").first.content
+    movie_title = movie_result.xpath("/film/nazev").text
+    made_data = movie_result.xpath("/film/zeme").text
     parsed_made_data = made_data.match(/^(.[^,]*), ([0-9]{4}), (?:.[^0-9])*([0-9]{1,3} min)/)
     movie_country = parsed_made_data[1]
     movie_year = parsed_made_data[2]
     movie_length = parsed_made_data[3]
-    movie_rating = movie_result.xpath("//rating").first.content
+    movie_rating = movie_result.xpath("/film/rating").text
     movie_link = "http://www.csfd.cz/film/#{movie_id}"
     "#{movie_title} (#{movie_year}), #{movie_country}, #{movie_length}, #{movie_rating}% - #{movie_link}"
   rescue
     "No result"
   end
 end
+
+class NoMovieFoundError < StandardError; end
