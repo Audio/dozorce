3ec3585d299fc7658413356033fee019d9252640
Vojtech Bocek <vbocek@gmail.com>
2013-04-20 23:25:27 +0200

    Remove unused/broken RSS feeds

--- dozorce.rb
+++ dozorce.rb
@@ -41,14 +41,12 @@ end
 
 Rss.configure do |c|
   c.feeds = {
-      :AppSales        => 'http://feeds.feedburner.com/AppsalesRss?format=xml',
       :bashoid         => 'https://github.com/Audio/bashoid/commits/master.atom',
       :dozorce         => 'https://github.com/Audio/dozorce/commits/master.atom',
       :DotaTimer       => 'https://github.com/Frca/DotaTimer/commits/master.atom',
       :fIRC            => 'https://github.com/Frca/fIRC/commits/master.atom',
       :Lorris          => 'https://github.com/Tasssadar/Lorris/commits/master.atom',
       :"Lorris mobile" => 'https://github.com/Tasssadar/lorris_mobile/commits/master.atom',
-      :MaNGOS          => 'https://github.com/mangos/server/commits/master.atom',
       :soulcore        => 'http://soulwell.czechdream.cz/rss_git.php?parameters=p%3Dsoulcore.git%3Ba%3Datom',
       :Steam           => 'http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=steam_games',
       :TrinityCore     => 'https://github.com/TrinityCore/TrinityCore/commits/master.atom',
