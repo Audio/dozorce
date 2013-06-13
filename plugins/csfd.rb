97ff26079f7168bdaf0b55b81a33d8510e3cf0fb
Frca <killerfrca@gmail.com>
2013-06-14 01:45:54 +0200

    Let csfd plugin use official api
    Signed-off-by: Frca <killerfrca@gmail.com>

--- plugins/csfd.rb
+++ plugins/csfd.rb
@@ -14,24 +14,23 @@ class Csfd
   end
 
   def search(query)
-    search_url = "http://csfd.matousskala.cz/api/hledat.php?q=#{CGI.escape(query)}"
-    search_result = WebPage.load_xml(search_url)
-    movie_id = search_result.xpath("//film[1]/id").text
-    raise NoMovieFoundError if movie_id.empty?
+    search_url = "http://csfdapi.cz/movie?search=#{CGI.escape(query)}"
+    search_result = WebPage.load_json(search_url)
+    raise NoMovieFoundError if search_result.length == 0
+    movie_id =  search_result[0][:id]
 
-    movie_url = "http://csfd.matousskala.cz/api/film.php?id=#{movie_id}"
-    movie_result = WebPage.load_xml(movie_url)
+    movie_url = "http://csfdapi.cz/movie/#{movie_id}"
+    movie_result = WebPage.load_json(movie_url)
 
-    movie_title = movie_result.xpath("/film/nazev").text
-    made_data = movie_result.xpath("/film/zeme").text
-    parsed_made_data = made_data.match(/^(.[^,]*), ([0-9]{4}), (?:.[^\w])*([\w]* min)/)
-    movie_country = parsed_made_data[1]
-    movie_year = parsed_made_data[2]
-    movie_length = parsed_made_data[3]
-    movie_rating = movie_result.xpath("/film/rating").text
-    movie_link = "http://www.csfd.cz/film/#{movie_id}"
+    movie_title = movie_result[:names][movie_result[:names].keys.first]
+    movie_country = movie_result[:countries].join(' / ')
+    movie_year = movie_result[:year]
+    movie_length = movie_result[:runtime]
+    movie_rating = movie_result[:rating]
+    movie_link = movie_result[:csfd_url]
+
     result = "#{movie_title} (#{movie_year}), #{movie_country}, #{movie_length}"
-    unless movie_rating.empty?
+    unless movie_rating.nil?
       result += ", #{movie_rating}%"
     end
     result += " - #{movie_link}"
