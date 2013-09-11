486504aa000c2c3012bfc2f52acfd96a94d61e4c
Audio <martin.foucek@gmail.com>
2013-09-11 21:34:43 +0200

    Fixes that previous "fix"

--- plugins/twitter.rb
+++ plugins/twitter.rb
@@ -15,7 +15,7 @@ class Twitter
   private
   def status(url_part)
     html = WebPage.load_html("https://twitter.com/#{url_part}")
-    tweet = html.css('.permalink-inner .tweet-text')[0].text
+    tweet = html.css('.permalink > .permalink-inner .tweet-text')[0].text
     tweet.gsub("\n", ' ')
   rescue OpenURI::HTTPError
     'Not found'
